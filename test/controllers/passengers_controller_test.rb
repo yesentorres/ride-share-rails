27f183b8f1b1aea78001f58a8abbb0397e0ec708
Alex Robertson <arobertson335@gmail.com>
2020-05-07 18:02:49 -0700

    Added Passengers controller.

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,31 +1,7 @@
 require "test_helper"
 
 describe PassengersController do
-  describe "index" do
-    # Your tests go here
-  end
-
-  describe "show" do
-    # Your tests go here
-  end
-
-  describe "new" do
-    # Your tests go here
-  end
-
-  describe "create" do
-    # Your tests go here
-  end
-
-  describe "edit" do
-    # Your tests go here
-  end
-
-  describe "update" do
-    # Your tests go here
-  end
-
-  describe "destroy" do
-    # Your tests go here
-  end
+  # it "does a thing" do
+  #   value(1+1).must_equal 2
+  # end
 end
