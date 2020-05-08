80265eea990665d2234b5032c2310efe35c15185
Alex Robertson <arobertson335@gmail.com>
2020-05-07 21:05:46 -0700

    Added passenger controller restful routes methods.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,5 +2,57 @@ class PassengersController < ApplicationController
 
   def index 
   end 
-  
+
+  def show
+    passenger_id = params[:id].to_i 
+    @passenger = Passenger.find_by(id: passenger_id) 
+
+    if @passenger.nil?
+      redirect_to root_path 
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+    elsif @passenger.update(
+      id: params[:passenger][:id],
+      name: params[:passenger][:name],
+      phone: params[:passenger][:phone]
+    )
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+
+  def edit 
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+    end
+  end
+
+  def destroy
+  end 
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(
+      id: params[:passenger][:id],
+      name: params[:passenger][:name],
+      phone: params[:passenger][:phone]
+      )
+    if @passenger.save
+      redirect_to @passenger
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,5 +2,5 @@ class TripsController < ApplicationController
 
   def index
   end 
-  
+
 end
