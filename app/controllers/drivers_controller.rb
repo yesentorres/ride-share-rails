cd5a561ddc896bc8d634a2ee41a2b19497ded8cd
Yesenia <yesentorres@gmail.com>
2020-05-09 16:00:14 -0700

    created files for driver views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,81 @@
 class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.create(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def edit
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+
+    if @driver.update(driver_params)
+      redirect_to drivers_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+
+    @driver.destroy
+
+    redirect_to drivers_path
+    return
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available_status)
+  end
+
 end
